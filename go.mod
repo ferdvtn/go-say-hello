7ee3d5b5f8c6bb943fee0f8f3f649a16752fbc1e
Ahmad Ferdian <ahmadferdian@Ahmads-MacBook-Air.local>
2022-07-06 11:29:23 +0700

    update to SayHello with name string parameter

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/ferdvtn/go-say-hello
+module github.com/ferdvtn/go-say-hello/v2
 
 go 1.18
--- hello.go
+++ hello.go
@@ -2,6 +2,6 @@ package gosayhello
 
 import "fmt"
 
-func SayHello() {
-	fmt.Println("Hello World..")
+func SayHello(name string) {
+	fmt.Println("Hello", name)
 }
